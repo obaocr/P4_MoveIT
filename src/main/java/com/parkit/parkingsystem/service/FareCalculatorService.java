ba4db1e569b5df6418e09d7a183740c59a1811c0
obaocr <for.oba@outlook.fr>
2020-02-28 19:35:16 +0100

    Fix Integration Tests Issues for ParkingDatabaseIT class

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,22 +10,27 @@ public class FareCalculatorService {
 		return Math.round(fare * 100)/100.0;
 	}
 	
+	// calculate the duration
+	private static double calculateDuration(Ticket ticket) {
+		long diff = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+        long diffDays = diff / (24 * 60 * 60 * 1000);
+        long diffHours = diff / (60 * 60 * 1000) % 24;
+        long diffMinutes = diff / (60 * 1000) % 60;
+        return (diffDays*24 + diffHours +  diffMinutes/60.0);
+	}
+	
+	
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
         
-        //OBA : Fix issue to calculate duration, round for the ticket price
-        
-        long diff = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
-        long diffDays = diff / (24 * 60 * 60 * 1000);
-        long diffHours = diff / (60 * 60 * 1000) % 24;
-        long diffMinutes = diff / (60 * 1000) % 60;
-        double duration = diffDays*24 + diffHours +  diffMinutes/60.0;
+        //OBA : Fix issue to calculate duration, round for the fare
+        double duration = calculateDuration(ticket);
         
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(roundFare(duration * Fare.CAR_RATE_PER_HOUR));
+            	ticket.setPrice(roundFare(duration * Fare.CAR_RATE_PER_HOUR));
                 break;
             }
             case BIKE: {
--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -18,48 +18,56 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
 
-    private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
-    private static ParkingSpotDAO parkingSpotDAO;
-    private static TicketDAO ticketDAO;
-    private static DataBasePrepareService dataBasePrepareService;
+	private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+	private static ParkingSpotDAO parkingSpotDAO;
+	private static TicketDAO ticketDAO;
+	private static DataBasePrepareService dataBasePrepareService;
 
-    @Mock
-    private static InputReaderUtil inputReaderUtil;
+	// static enlevé ?
+	@Mock
+	private InputReaderUtil inputReaderUtil;
 
-    @BeforeAll
-    private static void setUp() throws Exception{
-        parkingSpotDAO = new ParkingSpotDAO();
-        parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
-        ticketDAO = new TicketDAO();
-        ticketDAO.dataBaseConfig = dataBaseTestConfig;
-        dataBasePrepareService = new DataBasePrepareService();
-    }
+	@BeforeAll
+	private static void setUp() throws Exception {
+		parkingSpotDAO = new ParkingSpotDAO();
+		parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
+		ticketDAO = new TicketDAO();
+		ticketDAO.dataBaseConfig = dataBaseTestConfig;
+		dataBasePrepareService = new DataBasePrepareService();
+		// OBA : Purge de la database de test faite une seule fois !
+		dataBasePrepareService.clearDataBaseEntries();
+	}
 
-    @BeforeEach
-    private void setUpPerTest() throws Exception {
-        when(inputReaderUtil.readSelection()).thenReturn(1);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-        dataBasePrepareService.clearDataBaseEntries();
-    }
+	@BeforeEach
+	private void setUpPerTest() throws Exception {
+		// OBA modifié pour ne conserver que
+		when(inputReaderUtil.readSelection()).thenReturn(1);
+	}
 
-    @AfterAll
-    private static void tearDown(){
+	@AfterAll
+	private static void tearDown() {
 
-    }
+	}
 
-    @Test
-    public void testParkingACar(){
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-    }
+	@Test
+	public void testParkingACar() throws Exception {
+		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("AAAAA");
+		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		parkingService.processIncomingVehicle();
+		// TODO: check that a ticket is actualy saved in DB and Parking table is updated
+		// with availability
+	}
 
-    @Test
-    public void testParkingLotExit(){
-        testParkingACar();
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
-    }
+	@Test
+	public void testParkingLotExit() throws Exception {
+		when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+		ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		parkingService.processIncomingVehicle();
+		Thread.sleep(2000);
+		ParkingService parkingService2 = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+		parkingService2.processExitingVehicle();
+		// TODO: check that the fare generated and out time are populated correctly in
+		// the database
+	}
 
 }
